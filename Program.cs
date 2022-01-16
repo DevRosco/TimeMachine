7b76a2658d66d2d3f212014947be5e7d3fc8b658
Rostislav Valouch <rosta.valouch@gmail.com>
2022-01-16 08:41:29 +0000

    figure out the timer

--- Program.cs
+++ Program.cs
@@ -1,1 +1,23 @@
 ﻿//Time Machine 
+Timer timer;
+
+ void something()
+{
+ timer = new Timer(TimerCallback, null,1000,1000);
+
+}
+
+
+
+ void TimerCallback(object o)
+{
+    Console.WriteLine("hello world");
+    GC.Collect();
+ 
+   
+}
+
+    Console.WriteLine("where what");
+something();
+
+Console.Read();

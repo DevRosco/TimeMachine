86b11d017250bbe1ec9b31a1b1b05c0eab48782d
Rostislav Valouch <rosta.valouch@gmail.com>
2022-01-16 12:34:20 +0000

    add create new file with values

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@
 
 Timer timer;
 DateTime DateTime;
+int num1 = 10;
+int num2 = 20;
 
  void TikTak()
 {
@@ -16,6 +18,11 @@ DateTime DateTime;
     Console.WriteLine(DateTime);
  }
 
-TikTak();
+string message = String.Format("New Values {0}, {1}", num1, num2);
+
+//Just Playing with this now
+//so the plan is to add database - just a text file
+//save to file and read from file
+System.IO.File.WriteAllText(@"C:\Users\rosco\source\repos\TimeMachine\TextFile1.txt", message );
 
 Console.Read();
